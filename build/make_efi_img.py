826cd307f47840ae71c1d8ab629dc3e551602676
Jacek Konieczny <jajcus@jajcus.net>
2013-11-17 16:31:37 +0100

    make_efi_img: removing old unused code

--- build/make_efi_img.py
+++ build/make_efi_img.py
@@ -17,66 +17,6 @@ logger = logging.getLogger("make_efi_img")
 
 DU_OUTPUT_RE = re.compile("^(\d+)\s+total", re.MULTILINE)
 
-def install_grub(config, platform, lodev, boot_mnt_dir, grub_prefix,
-                                                            grub_early_fn):
-    logger.info("Installing GRUB for {0}".format(platform))
-    grub_prefix_dir = os.path.join(boot_mnt_dir, grub_prefix)
-    grub_plat_dir = os.path.join(grub_prefix_dir, platform)
-    if not os.path.exists(grub_plat_dir):
-        os.makedirs(grub_plat_dir)
-    if platform.endswith("-efi"):
-        efi_dir = os.path.join(boot_mnt_dir, "EFI/BOOT")
-        if not os.path.exists(efi_dir):
-            os.makedirs(efi_dir)
-        if "64" in platform:
-            efi_arch = "X64"
-        else:
-            efi_arch = "IA32"
-        grub_img_fn = os.path.join(efi_dir, "BOOT{}.EFI".format(efi_arch))
-    else:
-        grub_img_fn = os.path.join(grub_plat_dir, "core.img")
-        efi_arch = ""
-
-    with open(grub_early_fn, "wt") as grub_early:
-        grub_early.write("echo \"starting grub\"\n"
-                         "search.fs_uuid {} root\n"
-                         "set prefix=($root)/grub\n"
-                         .format(config.efi_vol_id))
-        if platform.endswith("-efi"):
-            # grub does not recognize partitions of the EFI eltorito image
-            # so let's try the other way round
-            grub_early.write("configfile $prefix/go_normal.cfg\n"
-                            "echo \"boot partition not found"
-                                    " falling back to loopback device\"\n"
-                            "search.fs_uuid {} cd\n"
-                            "loopback loop ($cd)/pld-nr-{}.img\n"
-                            "set root=(loop,msdos1)\n"
-                            "set prefix=($root)/grub\n"
-                            .format(efi_arch, config.cd_vol_id, config.bits))
-
-    grub_core_modules = ["search", "search_label", "fat", "part_msdos", "echo"]
-    if platform.endswith("-pc"):
-        grub_core_modules += ["biosdisk", "minicmd"]
-    elif platform.endswith("-efi"):
-        grub_core_modules += ["iso9660", "loopback", "configfile"]
-    subprocess.check_call(["grub-mkimage",
-                            "--output", grub_img_fn,
-                            "--format", platform,
-                            "--prefix", "/grub",
-                            "--config", grub_early_fn,
-                            ] + grub_core_modules)
-    if platform.endswith("-pc"):
-        shutil.copy("/lib/grub/{0}/boot.img".format(platform),
-                                os.path.join(grub_plat_dir, "boot.img"))
-        subprocess.check_call(["grub-bios-setup",
-                                "--directory", grub_plat_dir,
-                                lodev])
-    elif platform.endswith("-efi"):
-        subprocess.check_call(["grub-mkfont",
-                                "--output", os.path.join(grub_prefix_dir,"font.pf2"),
-                                "/usr/share/fonts/TTF/DejaVuSansMono.ttf"])
-    config.copy_dir("/lib/grub/{0}".format(platform), grub_plat_dir)
-
 def main():
     log_parser = pld_nr_buildconf.get_logging_args_parser()
     parser = argparse.ArgumentParser(description="Make EFI partition image",
