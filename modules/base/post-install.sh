51ee6e3fd99557c427e90055c3a747fb19a650da
Arkadiusz Miśkiewicz <arekm@maven.pl>
2025-06-30 15:32:16 +0200

    Install kernel-drm package.
    drm modules are installed now but blacklisted. You can delete blacklist
    file after a boot.
    
    Only bochs (mainly for Proxmox VM in UEFI as there seem to be no GOP
    support in Proxmox UEFI firmware, so no efifb video output) is allowed
    early in initrd.

--- modules/base/post-install.sh
+++ modules/base/post-install.sh
@@ -110,3 +110,9 @@ EOF
 cat >> root/etc/sysctl.d/pldnr_printk.conf <<'EOF'
 kernel.printk = 4	4	1	7
 EOF
+
+###########################################################
+# blacklist drm modules (only some are allowed early, in initrd)
+cat >> root/etc/modprobe.d/blacklist-drm.conf << 'EOF'
+install drm /bin/false
+EOF
