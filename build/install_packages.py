091f6b6e82d840e8259b254a1faf0074418ac76e
Arkadiusz Miśkiewicz <arekm@maven.pl>
2026-03-05 21:53:59 +0100

    Workaround for broken ('no digest') rpm packages in PLD.

--- build/install_packages.py
+++ build/install_packages.py
@@ -60,6 +60,7 @@ class PackageInstaller(object):
                                 + ["poldek", "--root", self.dst_dir,
                                 "--conf", "poldek.conf",
                                 "--cachedir", self.cache_dir,
+                                "--pmopt='--nodigest'",
                                 "-O", "rpmdef=_netsharedpath ''" ] \
                                 + self.langs_opts + list(args)
             logger.debug("Running: {0}".format(cmd))
